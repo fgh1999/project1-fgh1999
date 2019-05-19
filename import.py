d987d6948e04cad8b78055b0e79636c292544bb7
fgh1999 <fangaunghong1999@163.com>
2019-05-19 18:15:02 +0800

    simplify the cv read process in import.py

--- import.py
+++ import.py
@@ -1,11 +1,11 @@
-import pandas as pd
+import csv
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
-engine = create_engine('postgresql://postgres:123456@127.0.0.1:5432/postgres')
+
 
 class BookRecord(Base):
     __tablename__ = 'book_record'
@@ -14,20 +14,19 @@ class BookRecord(Base):
     author = Column(String(200), nullable=False)
     year = Column(Integer, nullable=False)
 
-# read data
-df = pd.read_csv('books.csv')
 
+engine = create_engine('postgresql://postgres:123456@127.0.0.1:5432/postgres')
 Base.metadata.create_all(engine)
-
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-## Insert data into table 'book_record'
-for index, row in df.iterrows():
-    record = BookRecord(isbn=row['isbn']
-                        , title=row['title']
-                        , author=row['author']
-                        , year=row['year'])
-    session.add(record)
+# read data
+with open('books.csv') as file:
+    rows = csv.reader(file)
+    next(rows)
+    for row in rows:
+        record = BookRecord(isbn=row[0], title=row[1],
+                            author=row[2], year=int(row[3]))
+        session.add(record)
 
 session.commit()
